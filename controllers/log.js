dd91f1fed14cbe995bd004d74b42bb5458a1329f
Nick Van Doorn <vandoorn.nick@gmail.com>
2016-11-19 01:33:32 -0800

    Added comments

--- controllers/log.js
+++ controllers/log.js
@@ -11,6 +11,11 @@ const router = express.Router();
 const LOG_DIR = '/var/log/clientlog';
 const DEFAULT_START_TIME_HR = 8;
 
+/**
+ *  Enum for log levels
+ *
+ * @type {{ERROR: number, WARN: number, INFO: number, VERBOSE: number, DEBUG: number}}
+ */
 exports.levels = {
     ERROR: 0,
     WARN: 1,
@@ -19,6 +24,17 @@ exports.levels = {
     DEBUG: 4
 };
 
+/**
+ * Mapping to CSS classes from debug level
+ *
+ * @param level: string
+ *  String from parsed log
+ *  TODO
+ *  should be an emum
+ *
+ * @returns: string
+ *  CSS class string
+ */
 exports.getClass = (level) => {
     let levelStr;
     if(helpers.isUndefined(level)){
@@ -30,6 +46,14 @@ exports.getClass = (level) => {
     return util.format('logline-%s', levelStr.toLowerCase());
 };
 
+/**
+ * Generates JSON object for a given log file
+ *
+ * @param logPath: string
+ *  Path to log file
+ * @returns: {Promise}
+ *  Resolves with a list of JSON objects
+ */
 exports.generateLogEntries = (logPath) => {
     return new Promise((resolve, reject) => {
         async.waterfall([
@@ -71,6 +95,14 @@ exports.generateLogEntries = (logPath) => {
     });
 };
 
+/**
+ * Walks a given directory for log files (used in sidebar)
+ *
+ * @param logDir: string
+ *  Path to log direcory
+ * @returns: {Promise}
+ *  Resolves with a list of lognames
+ */
 exports.getLogFileNames = (logDir) => {
     return new Promise((resolve, reject) => {
         let logFileNames = [];
@@ -87,41 +119,73 @@ exports.getLogFileNames = (logDir) => {
 
 };
 
-exports.LogFile = function(logPath){
-    const self = this;
-    exports.generateLogEntries(logPath).then((data) => {
-        self.logEntries = data;
-        self.emit('ready');
-    });
-};
-
-exports.LogFile.prototype.__proto__ = events.EventEmitter.prototype;
-
-exports.LogFile.prototype.query = function(query){
-    const self = this;
-    return new Promise((resolve, reject) => {
-        let startTimestamp = helpers.isUndefined(query.startdt) ? new Date().getTime() - 60 * 60 * 1000 * DEFAULT_START_TIME_HR
-            : new Date(query.startdt).getTime();
-        let endTimestamp = helpers.isUndefined(query.enddt) ? new Date().getTime() : new Date(query.enddt).getTime();
-        let startLine = query.startline || 0;
-        let endLine = query.endline || self.logEntries.length;
-        let level = parseInt(query.level) || exports.levels.DEBUG;
-        let filtered = self.logEntries.filter((entry) => {
-            let timeMatch = entry.time >= startTimestamp && entry.time <= endTimestamp;
-            let levelMatch = entry.level <= level;
-            let lineMatch = self.logEntries.indexOf(entry) >= startLine && self.logEntries.indexOf(entry) <= endLine;
-            return timeMatch && levelMatch && lineMatch;
+/**
+ * Models a given LogFile
+ */
+class LogFile {
+    /**
+     * Constructor generates a list of logEntries
+     * for a given log file
+     *
+     * @param logPath: string
+     *  Path to log file
+     */
+    constructor(logPath) {
+        const self = this;
+        exports.generateLogEntries(logPath).then((data) => {
+            self.logEntries = data;
+            self.emit('ready');
         });
-        resolve(filtered);
-    });
-};
+    // Extends EventEmitter
+    this.__proto__ = events.EventEmitter.prototype;
+    }
 
-exports.LogFile.prototype.getAll = function(){
-    return this.logEntries;
-};
+    /**
+     * Queries the list this list of fileEntries
+     *
+     * TODO
+     * Defined this API better
+     *
+     * @param reqQuery: JSON object
+     *  Query params (probably from express)
+     * @returns {Promise}
+     *  Resolves to a list of JSON objects
+     */
+    query(reqQuery) {
+        const self = this;
+        return new Promise((resolve, reject) => {
+            let startTimestamp = helpers.isUndefined(reqQuery.startdt) ? new Date().getTime() - 60 * 60 * 1000 * DEFAULT_START_TIME_HR
+                : new Date(reqQuery.startdt).getTime();
+            let endTimestamp = helpers.isUndefined(reqQuery.enddt) ? new Date().getTime() : new Date(reqQuery.enddt).getTime();
+            let startLine = reqQuery.startline || 0;
+            let endLine = reqQuery.endline || self.logEntries.length;
+            let level = parseInt(reqQuery.level) || exports.levels.DEBUG;
+            let filtered = self.logEntries.filter((entry) => {
+                let timeMatch = entry.time >= startTimestamp && entry.time <= endTimestamp;
+                let levelMatch = entry.level <= level;
+                let lineMatch = self.logEntries.indexOf(entry) >= startLine && self.logEntries.indexOf(entry) <= endLine;
+                return timeMatch && levelMatch && lineMatch;
+            });
+            resolve(filtered);
+        });
+    }
 
+    /**
+     * Get all these logEntries
+     *
+     * @returns: list of JSON objects
+     *  List of logEntry JSON objects
+     */
+    getAll() {
+        return this.logEntries;
+    }
+}
+
+/**
+ * Only route so far
+ */
 router.get('/:logfile', (req, res, next) => {
-    let logFile = new exports.LogFile(req.params.logfile);
+    let logFile = new LogFile(req.params.logfile);
     logFile.on('ready', () => {
         let promises = [
             logFile.query(req.query),
@@ -140,7 +204,6 @@ router.get('/:logfile', (req, res, next) => {
 
 module.exports = router;
 
-exports.getLogFileNames(LOG_DIR);
 
 
 
