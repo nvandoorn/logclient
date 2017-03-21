8d62478f9bdab75297b6d833cb22b92d33d9bcf2
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-03-20 21:53:44 -0700

    Majority of line parsing implemented
    Updated readme to add patterns to config blob
    Wrote some janky code to parse log lines
    Added dateformat dependency

--- src/server/models/logfile.js
+++ src/server/models/logfile.js
@@ -8,15 +8,43 @@ const util = require('util');
 const walk = require('walk');
 const Heap = require('heap');
 const clone = require('clone');
+const dateformat = require('dateformat');
 
 const helpers = require('../helpers/helpers');
 const constants = require('../helpers/constants');
 const lineParser = require('../helpers/lineparser');
 
-function parseLine(line, timePattern, levelPattern){
-  const timeRegex = new RegExp(timePattern);
-  const levelRegex = new RegExp(levelPattern);
-  
+const DEFAULT_LEVEL = constants.DEBUG;
+const DEFAULT_LEVEL_STR = 'debug'; // TODO less jank here
+
+function parseLine(line, datetimePattern, levelPattern, datetimeFormatter){
+  const dateSplit = line.split(datetimePattern);
+  const levelSplit = line.split(levelPattern);
+  const levelObj = getLevel(levelSplit[0], constants.levels);
+  const dateObj = new Date(dateSplit[0]);
+  return {
+    text: levelSplit[1],
+    level: levelObj.level,
+    levelStr: levelObj.str,
+    date: dateObj.getTime(),
+    dateStr: dateFormat(dateObj, datetimeFormatter)
+  }
+}
+
+// TODO remove this filthy jank
+function getLevel(levelStr, levelEnum){
+  for(let key in levelEnum){
+    if(levelStr.trim().toLowerCase().includes(key.toLowerCase())){
+      return {
+        level: levelEnum[key],
+        str: key.toLowerCase()
+      }
+    }
+  }
+  return {
+    level: DEFAULT_LEVEL,
+    str: DEFAULT_LEVEL_STR
+  }
 }
 
 const Logfile = {
@@ -28,8 +56,9 @@ const Logfile = {
   readFile: function(){
     fs.readFile(this.filepath, (err, data) => {
       if(err) throw err;
-      this.loglines = parseLines(data, this.timePattern, this.levelPattern);
-    })
+      this.loglines = parseLines(data, this.config.datetimePattern,
+                      this.config.levelPattern, this.config.timeFormatter);
+    });
   }
   query: function(queryParmas){
 
@@ -39,133 +68,4 @@ const Logfile = {
   }
 };
 
-/**
- * Models a given LogFile
- */
-class LogFile extends events.EventEmitter{
-  /**
-   * Generates JSON object for a given log file
-   *
-   * @param logPath: string
-   *  Path to log file
-   * @returns: {Promise}
-   *  Resolves with a list of JSON objects
-   */
-  static generateLogEntries(logPath){
-     return new Promise((resolve, reject) => {
-      async.waterfall([
-          (next) => {
-            fs.readFile(logPath, (err, data) => {
-              next(null, data.toString());
-            });
-          },
-          (data, next) => {
-            let split = data.split(constants.isoDtExp);
-            let lines = [];
-            for(let i = 1; i < split.length; i += 2){
-              lines.push(split[i] + split[i+1]);
-            }
-            next(null, lines);
-          },
-          (data, done) => {
-            done(null, lineParser.parseLines(data));
-          }],
-        (err, results) => {
-          resolve(results);
-        }
-      );
-    });
-  }
-
-  static paginate(toPaginate, pageSize){
-    pageSize = parseInt(pageSize) || constants.defaultPageSize;
-    let toPaginateHeap = LogFile.lineEntryHeapSort(toPaginate);
-    let toReturn = [];
-    while(!toPaginateHeap.empty()){
-      let chunk = [];
-      while(chunk.length < pageSize && !toPaginateHeap.empty()){
-        chunk.push(toPaginateHeap.pop());
-      }
-      toReturn.push(chunk.reverse());
-    }
-    return toReturn;
-  }
-
-  static lineEntryHeapSort(lineEntries){
-    let heap = new Heap((a, b) => {
-      return b.line - a.line;
-    });
-    while(lineEntries.length > 0){
-      heap.push(lineEntries.pop());
-    }
-    heap.heapify();
-    return heap;
-  }
-
-  /**
-   * Constructor generates a list of logEntries
-   * for a given log file
-   *
-   * @param logPath: string
-   *  Path to log file
-   */
-  constructor(logPath) {
-    super();
-    LogFile.generateLogEntries(logPath).then((data) => {
-      this.logEntries = data;
-      this.emit('ready');
-    });
-  }
-
-  /**
-   * Queries the list this list of fileEntries
-   *
-   * TODO
-   * Defined this API better
-   *
-   * @param reqQuery: JSON object
-   *  Query params (probably from express)
-   * @returns {Promise}
-   *  Resolves to a list of JSON objects
-   */
-  query(reqQuery) {
-    return new Promise((resolve, reject) => {
-      let level;
-      try{
-        level = parseInt(reqQuery.level);
-        if(isNaN(level)){
-          throw new Exception('level must be int')
-        }
-      }
-      catch(err){
-        level = constants.levels.DEBUG;
-      }
-      let startLine = parseInt(reqQuery.startline) || 0;
-      let endLine = parseInt(reqQuery.endline) || this.logEntries.size();
-      let startTimeStr = reqQuery.startdt || 0;
-      let endTimeStr = reqQuery.enddt || new Date().getTime();
-      let startTimestamp = new Date(startTimeStr).getTime();
-      let endTimestamp = new Date(endTimeStr).getTime();
-      let filtered = this.logEntries.clone();
-      resolve(filtered.toArray().filter((entry) => {
-        let levelMatch = entry.level <= level;
-        let dateMatch = entry.time <= endTimestamp && entry.time >= startTimestamp;
-        let lineMatch = entry.line <= endLine && entry.line >= startLine;
-        return lineMatch && levelMatch && dateMatch;
-      }));
-    });
-  }
-
-  /**
-   * Get all these logEntries
-   *
-   * @returns: list of JSON objects
-   *  List of logEntry JSON objects
-   */
-  getAll() {
-    return this.logEntries;
-  }
-
-}
-
-module.exports = LogFile;
+module.exports = Logfile;
