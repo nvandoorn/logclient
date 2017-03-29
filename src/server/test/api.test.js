96403c04c070cd6aaa398d315a4ddb90dd0138b8
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-03-28 23:09:04 -0700

    Cleanup API tests and add new test

--- src/server/test/api.test.js
+++ src/server/test/api.test.js
@@ -47,13 +47,14 @@ describe('REST API', function(){
   describe('#directory()', function(){
     it(`should contain ${LOGFILE_NAME}`, function(){
       return get(DIRECTORY_ROUTE).then(body => {
-        assert.include(body.data, LOGFILE_NAME);
+        assert.include(body.data, LOGFILE_NAME, `directory list did not include ${LOGFILE_NAME}`);
       })
     })
   });
 
   describe('#query()', function(){
     const thisGet = () => get(FILE_ROUTE, { logfile: LOGFILE_NAME });
+    const bodyFailMsg = 'query request failed';
     it('should correctly parse the first line', function(){
       const correct = {
         text: `You must be pretty desperate if you're looking at the logs`,
@@ -63,21 +64,32 @@ describe('REST API', function(){
         datetimeStr: '6:50:40'
       };
       return thisGet().then(body => {
-        assert(body.success);
+        assert(body.success, bodyFailMsg);
         assert.deepEqual(body.data[0], correct, 'did not correctly parse logline');
       });
     });
 
+    it('should return an empty list for a future date range', function(){
+      return get(FILE_ROUTE, {
+        logfile: LOGFILE_NAME,
+        startdt: Date.now() + 1 * 1000 * 60,
+        enddt: Date.now() + 2 * 1000 * 60
+      }).then(body => {
+        assert(body.success, bodyFailMsg);
+        assert(!body.data, 'returned non-empty list for future date query'); // TODO this should check length
+      })
+    })
+
     it('should default level on unknown level', function(){
       return thisGet().then(function(body){
-        assert(body.success);
+        assert(body.success, bodyFailMsg);
         assert.strictEqual(body.data[4].level, constants.defaultLevel, 'level did not default');
       });
     });
 
     it('should default level string on unknown level', function(){
       return thisGet().then(function(body){
-        assert(body.success);
+        assert(body.success, bodyFailMsg);
         assert.strictEqual(body.data[4].levelStr, constants.defaultLevelStr,
             'level string did not defualt');
       })
