b0f37f657b78db54f430fd7a827a5df792ccd1d4
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-03-30 21:31:46 -0700

    Convert levels to lowercase

--- src/server/helpers/constants.js
+++ src/server/helpers/constants.js
@@ -6,14 +6,14 @@
  * @type {{ERROR: number, WARN: number, INFO: number, VERBOSE: number, DEBUG: number}}
  */
 exports.levels = {
-  ERROR: 0,
-  WARN: 1,
-  INFO: 2,
-  VERBOSE: 3,
-  DEBUG: 4
+  error: 0,
+  warn: 1,
+  info: 2,
+  verbose: 3,
+  debug: 4
 };
 
-exports.defaultLevel = exports.levels.DEBUG;
+exports.defaultLevel = exports.levels.debug;
 
 exports.defaultLevelStr = 'debug';
 
