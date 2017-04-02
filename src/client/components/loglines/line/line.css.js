224fb36b22b2712d6ac54cda442ba14bfc0fead1
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-04-01 17:26:29 -0700

    Map colour on level in line

---
+++ src/client/components/loglines/line/line.css.js
@@ -0,0 +1,26 @@
+import { colours } from '../../../globals.config';
+
+export const left = {
+  display: 'inline-block'
+};
+
+export const text = {
+  display: 'inline-block'
+};
+
+export const debug = {
+  color: colours.loglevels.debug
+};
+
+export const info = {
+  color: colours.loglevels.info
+};
+
+export const warn = {
+  color: colours.loglevels.warn
+};
+
+export const error = {
+  color: colours.loglevels.error
+};
+
--- src/client/components/loglines/line/line.js
+++ src/client/components/loglines/line/line.js
@@ -1,12 +1,34 @@
 import React from 'react';
 
+import { levels, defaultLevel } from '../../../../constants'
+
+import { left, text, debug, info, warn, error } from './line.css';
+
+function getLevelStyle(level, levelEnum){
+  switch(level){
+    case levelEnum.debug:
+      return debug;
+    break;
+    case levelEnum.info:
+      return info;
+    break;
+    case levelEnum.warn:
+      return warn
+    break;
+    case levelEnum.error:
+      return error
+    default:
+      return defaultLevel
+  }
+}
+
 const Line = ({props}) =>
   <li className={props.classStr}>
-    <div className="left">
-      <span className="level">{props.levelStr}</span>
-      <span className="datetime">{props.datetimeStr}</span>
+    <div className={left}>
+      <div className="datetime">{props.datetimeStr}</div>
+      <div className={getLevelStyle(props.level, levels)}>{props.levelStr}</div>
     </div>
-    <div className="text">{props.text}</div>
+    <div className={text}>{props.text}</div>
   </li>;
 
 export default Line;
