a96fe85e433e1c513d807dbb40497e1406fc2a39
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-04-02 17:37:03 -0700

    Use janky css global export system

--- src/client/globals.config.js
+++ src/client/globals.config.js
@@ -1,10 +1,14 @@
+// TODO figure out why import/export is borked in this file
+const levels = require('../constants').levels;
+
 exports.colours = {
-  loglevels: {
-    debug: '#34989F',
-    info: '#58B3FC',
-    warn: '#F0C850',
-    error: '#F35656'
-  },
+ loglevels: [
+    { level: levels.debug, colour: '#34989F' },
+    { level: levels.info, colour: '#58B3FC' },
+    { level: levels.warn, colour: '#F0C850' },
+    { level: levels.error, colour: '#F35656' },
+    { level: levels.verbose, colour: '#51F2FC' }
+  ],
   background: {
     main: '#40454E',
     accent: '#262A32'
