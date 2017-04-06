2cf2e6aff2c2697ecbff410a33b03af5accf90d3
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-04-05 21:59:16 -0700

    Add jsdom test to loglines

--- src/client/components/loglines/loglines.test.js
+++ src/client/components/loglines/loglines.test.js
@@ -1,18 +1,25 @@
 /* eslint-env jest */
 
 import React from 'react'
+import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
 
 import Loglines from './loglines'
 
-it('renders correctly', () => {
-  const props = [{
-    text: `You must be pretty desperate if you're looking at the logs`,
-    level: 2,
-    levelStr: 'info',
-    datetime: 1489387840000,
-    datetimeStr: '6:50:40'
-  }]
+const props = [{
+  text: `You must be pretty desperate if you're looking at the logs`,
+  level: 2,
+  levelStr: 'info',
+  datetime: 1489387840000,
+  datetimeStr: '6:50:40'
+}]
+
+it('should render correctly', () => {
   const loglines = renderer.create(<Loglines loglines={props} />).toJSON()
   expect(loglines).toMatchSnapshot()
+})
+
+it('should render without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(<Loglines loglines={props} />, div)
 })
