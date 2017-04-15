a9c2dd87098935f4db1c3cb4a5b1e2eb9fd2fde3
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-04-14 20:21:12 -0700

    Refactor build scripts

--- scripts/start.js
+++ scripts/start.js
@@ -1,8 +1,16 @@
 'use strict'
 
 const createServer = require('./server')
-const build = require('./build')
-const port = parseInt(process.env.PORT || 3000)
+const fs = require('fs-extra')
+const path = require('path')
 
-build()
-createServer(port)
+const PORT = process.env.PORT || 3000
+const INDEX_PATH = path.join(__dirname, '../build/index.html')
+
+fs.stat(INDEX_PATH, (err, stats) => {
+  if (!err && stats.isFile()) createServer(PORT)
+  else {
+    console.log(`Failed to serve ${INDEX_PATH}. Try running 'npm run build' and try again`)
+    process.exit(1)
+  }
+})
