720c53ce92cdc8f7fae1cf9d0176191a4f9a7f77
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-04-19 22:06:16 -0700

    Remove promise constructor and handle not ready

--- src/server/models/directory.js
+++ src/server/models/directory.js
@@ -6,33 +6,39 @@ const path = require('path')
 const Logfile = require('./logfile')
 const buildRes = require('../helpers/build-res')
 
+const notReadyReply = dir => buildRes(false, `Directory ${dir} is not ready yet`, {})
+
 const Directory = {
   create (dirPath, config) {
-    return Object.assign({
+    const instance = Object.assign({
       dirPath: dirPath,
       config: config
-    }, this).readDir()
+    }, this)
+    instance.readDir()
+    return instance
   },
   readDir () {
-    return new Promise((resolve, reject) => {
-      fs.readdir(this.dirPath, (err, list) => {
-        if(err) reject(new Error(`Failed to list ${this.dirPath}: ${err.message}`))
-        this.filelist = list.map((name, i) => ({ name: name, key: i }))
-        Promise.all(list.map((name, i) => Logfile.create(path.join(this.dirPath, name), this.config, i)))
-        .then(logfiles => {
-          this.logfiles = logfiles
-          resolve(this)
-        })
-      })
+    fs.readdir(this.dirPath, (err, list) => {
+      if(err) reject(new Error(`Failed to list ${this.dirPath}: ${err.message}`))
+      this.filelist = list.map((name, i) => ({ name: name, key: i }))
+      Promise.all(list.map((name, i) => Logfile.create(path.join(this.dirPath, name), this.config, i)))
+      .then(logfiles => {
+        this.logfiles = logfiles
+      }, err => { reject(err)  })
     })
   },
   query (params) {
-    const logfile = this.logfiles.find(k => k.key === params.key)
-    if(logfile) return logfile.query(params)
-    else return buildRes(false, `Failed to find logfile with key ${params.key}`, {})
+    if (this.logfiles) {
+      const logfile = this.logfiles.find(k => k.key === params.key)
+      if(logfile) return logfile.query(params)
+      else return buildRes(false, `Failed to find logfile with key ${params.key}`, {})
+    } else {
+      return notReadyReply(this.dirPath)
+    }
   },
   list () {
-    return buildRes(true, `Retrieved listing for ${this.dirPath}`, this.filelist)
+    return this.filelist ?
+      buildRes(true, `Retrieved listing for ${this.dirPath}`, this.filelist) : notReadyReply(this.dirPath)
   }
 }
 
