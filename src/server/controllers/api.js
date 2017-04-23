bf2d2446afc952efdb18a630c1568672b437c45f
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-04-22 23:47:58 -0700

    Wrap API routing in function
    I'm still having some terrible blocking issue with the express router
    but I'm pressing on for now. Going forward I would like to seperate
    routing from this business logic.
    
    In the meantime I started work on POST /api/directory so the active
    directory can be changed

--- src/server/controllers/api.js
+++ src/server/controllers/api.js
@@ -4,19 +4,21 @@ const express = require('express')
 const router = express.Router()
 const path = require('path')
 const blocked = require('blocked')
+const async = require('async')
+const _ = require('lodash')
 
 const constants = require('../../constants')
 const buildRes = require('../helpers/build-res')
 
 const Config = require('../models/config')
-const Logfile = require('../models/logfile')
 const Directory = require('../models/directory')
 
 const CONFIG_PATH = path.join(__dirname, '../../../config.json')
+const CONFIG_ROUTE = '/config'
 const FILE_ROUTE = '/file'
 const DIR_ROUTE = '/directory'
 
-const normalizeFileReq = req => ({
+const normalizeReq = req => ({
   key: parseInt(req.query.key),
   pagenum: req.query.pagenum || 1,
   pagesize: parseInt(req.query.pagesize) || constants.defaultPageSize,
@@ -25,28 +27,49 @@ const normalizeFileReq = req => ({
   level: !isNaN(parseInt(req.query.level)) ? parseInt(req.query.level) : constants.defaultLevel
 })
 
-router.route('/config')
-  .get((req, res) => {
-    res.json(config.read())
+// TODO seperate routing from business logic
+function bindRoutes (router) {
+  const config = Object.assign({
+    configPath: CONFIG_PATH,
+    blob: {}
+  }, Config)
+  config.read()
+
+  let dir = Object.assign({
+    dirPath: config.blob.directories.find(k => k.active).path,
+    config: config.blob
+  }, Directory)
+  dir.readDir()
+
+  router.route(CONFIG_ROUTE)
+    .get((req, res) => {
+      res.json(config.read())
+    })
+    .put((req, res) => {
+      res.json(config.set(req.body))
+    })
+
+  router.get(FILE_ROUTE, (req, res) => {
+    res.json(dir.query(normalizeReq(req)))
   })
-  .put((req, res) => {
-    res.json(config.set(req.body))
-  })
 
-// TODO rid thee global scope
-const config = Config.create(CONFIG_PATH)
-const dir = Object.assign({
-  dirPath: config.blob.directories.find(k => k.active).path,
-  config: config.blob
-}, Directory)
-dir.readDir()
+  router.route(DIR_ROUTE)
+    .get((req, res) => {
+      res.json(dir.list())
+    })
+    .post((req, res) => {
+      // TODO set entry as active
+      const dirPath = config.blob.directories.find(k => k.key === parseInt(req.body.key)).path
+      dir = Object.assign({
+        dirPath: dirPath,
+        config: config.blob
+      }, Directory)
+      dir.readDir()
+      res.json(buildRes(true, 'we did stuff', {})) // TODO reply with status of change
+    })
 
-router.get(FILE_ROUTE, (req, res) => {
-  res.json(dir.query(normalizeFileReq(req)))
-})
+}
 
-router.get(DIR_ROUTE, (req, res) => {
-  res.json(dir.list())
-})
+bindRoutes(router)
 
 module.exports = router
