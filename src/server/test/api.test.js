242ff3d7593f97450854543b543c24b7cb2e316f
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-04-26 21:52:41 -0700

    Small (but working) test suite for new API

--- src/server/test/api.test.js
+++ src/server/test/api.test.js
@@ -2,43 +2,36 @@
 'use strict'
 
 const assert = require('chai').assert
-const path = require('path')
-
 const axios = require('axios')
 const subset = require('json-subset')
-
-const get = (route, params) => axios.get(route, { params: params })
-const post = axios.post
 
 const constants = require('../../constants')
 const ROUTES = require('../routes').routes
 const createServer = require('../../../scripts/server')
 
-const LOGFILE_PATH = path.join(__dirname, './fixtures')
 const TEST_PORT = 5000
 const BASE_ROUTE = `http://localhost:${TEST_PORT}/api`
 const CONFIG_ROUTE = BASE_ROUTE + ROUTES.CONFIG
 
 describe('REST API', function () {
-  const server = createServer(TEST_PORT); // eslint-disable-line
+  jasmine.DEFAULT_TIMEOUT_INTERVAL = 20 * 1000  // eslint-disable-line
+  const server = createServer(TEST_PORT) // eslint-disable-line
   describe('#config()', function () {
     const config = {
       datetimePattern: constants.defaultDatetimePattern.toString().slice(1, -1),
       levelPattern: constants.defaultLevelPattern.toString().slice(1, -1),
-      timeFormatter: constants.defaultTimeFormatter,
-      folders: [{
-        name: 'Test Fixture',
-        directory: LOGFILE_PATH
-      }]
+      timeFormatter: constants.defaultTimeFormatter
     }
-    it('should successfully set the config', function () {
-      console.log(expect(post(CONFIG_ROUTE, config)))
-    })
-    it('should successfully get a superset of the same config', function () {
-      return get(CONFIG_ROUTE).then(body => {
-        assert(body.data.success, 'failed to get config')
-        assert(subset(config, body.data), 'configs are not equal')
+    test('should successfully set the config', function () {
+      return axios.post(CONFIG_ROUTE, config).then(resp => {
+        assert(resp.data.success, 'should return success')
       })
     })
+    test('should successfully get a superset of the same config', function () {
+      return axios.get(CONFIG_ROUTE).then(body => {
+        assert(body.data.success, 'failed to get config')
+        assert(subset(config, body.data.data), 'config is not a superset')
+      })
+    }, 30 * 1000)
   })
 })
