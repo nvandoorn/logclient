a1400169a91ce7135cbed15742594f205df8b1a4
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-04-26 22:27:50 -0700

    Readd axios wrapper

--- src/server/test/api.test.js
+++ src/server/test/api.test.js
@@ -2,8 +2,11 @@
 'use strict'
 
 const assert = require('chai').assert
-const axios = require('axios')
 const subset = require('json-subset')
+
+const axios = require('axios')
+const get = (route, params) => axios.get(route, { params: params })
+const post = axios.post
 
 const constants = require('../../constants')
 const ROUTES = require('../routes').routes
@@ -23,15 +26,15 @@ describe('REST API', function () {
       timeFormatter: constants.defaultTimeFormatter
     }
     test('should successfully set the config', function () {
-      return axios.post(CONFIG_ROUTE, config).then(resp => {
+      return post(CONFIG_ROUTE, config).then(resp => {
         assert(resp.data.success, 'should return success')
       })
     })
     test('should successfully get a superset of the same config', function () {
-      return axios.get(CONFIG_ROUTE).then(body => {
+      return get(CONFIG_ROUTE).then(body => {
         assert(body.data.success, 'failed to get config')
         assert(subset(config, body.data.data), 'config is not a superset')
       })
-    }, 30 * 1000)
+    })
   })
 })
