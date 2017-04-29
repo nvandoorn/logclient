0de9b58acc0b98a948f92b569f055302f50da026
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-04-28 22:46:50 -0700

    Refactor test routing into function

--- src/server/test/api.test.js
+++ src/server/test/api.test.js
@@ -14,7 +14,8 @@ const createServer = require('../../../scripts/server')
 
 const TEST_PORT = 5000
 const BASE_ROUTE = `http://localhost:${TEST_PORT}/api`
-const CONFIG_ROUTE = BASE_ROUTE + ROUTES.CONFIG
+let JOINED_ROUTES = {}
+Object.keys(ROUTES).forEach(k => { JOINED_ROUTES[k] = BASE_ROUTE + ROUTES[k] }) // TODO make accessible
 
 describe('REST API', function () {
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 60 * 1000  // eslint-disable-line
@@ -25,15 +26,22 @@ describe('REST API', function () {
       levelPattern: constants.defaultLevelPattern.toString().slice(1, -1),
       timeFormatter: constants.defaultTimeFormatter
     }
-    test('should successfully set the config', function () {
-      return post(CONFIG_ROUTE, config).then(resp => {
+    it('should successfully set the config', function () {
+      return post(JOINED_ROUTES.CONFIG, config).then(resp => {
         assert(resp.data.success, 'should return success')
       })
     })
-    test('should successfully get a superset of the same config', function () {
-      return get(CONFIG_ROUTE).then(body => {
+    it('should successfully get a superset of the same config', function () {
+      return get(JOINED_ROUTES.CONFIG).then(body => {
         assert(body.data.success, 'failed to get config')
         assert(subset(config, body.data.data), 'config is not a superset')
+      })
+    })
+  })
+  describe('#directories', function () {
+    it('should get a list of files', function () {
+      return get(JOINED_ROUTES.DIRECTORIES).then(body => {
+        assert(body.data.success, 'failed to get directory listing')
       })
     })
   })
