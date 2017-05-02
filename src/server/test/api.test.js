06a0e405e50b7e458f9903724a472e49e5e4960e
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-05-01 22:05:37 -0700

    Implement more (WIP) /directories tests

--- src/server/test/api.test.js
+++ src/server/test/api.test.js
@@ -7,6 +7,8 @@ const subset = require('json-subset')
 const axios = require('axios')
 const get = (route, params) => axios.get(route, { params: params })
 const post = axios.post
+const put = axios.put
+const del = axios.delete
 
 const constants = require('../../constants')
 const getJoinedRoutes = require('../../helpers').getJoinedRoutes
@@ -19,6 +21,7 @@ const joinedRoutes = getJoinedRoutes(BASE_ROUTE)
 describe('REST API', function () {
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 60 * 1000  // eslint-disable-line
   const server = createServer(TEST_PORT) // eslint-disable-line
+
   describe('#config()', function () {
     const config = {
       datetimePattern: constants.defaultDatetimePattern.toString().slice(1, -1),
@@ -31,16 +34,37 @@ describe('REST API', function () {
       })
     })
     it('should successfully get a superset of the same config', function () {
-      return get(joinedRoutes.config).then(body => {
-        assert(body.data.success, 'failed to get config')
-        assert(subset(config, body.data.data), 'config is not a superset')
+      return get(joinedRoutes.config).then(resp => {
+        assert(resp.data.success, 'failed to get config')
+        assert(subset(config, resp.data.data), 'config is not a superset')
       })
     })
   })
+
   describe('#directories', function () {
-    it('should get a list of files', function () {
-      return get(joinedRoutes.directories).then(body => {
-        assert(body.data.success, 'failed to get directory listing')
+    it('should get a list of directories', function () {
+      return get(joinedRoutes.directories).then(resp => {
+        assert(resp.data.success, 'failed to get directory listing')
+        expect(resp.data).toMatchSnapshot()
+      })
+    })
+    it('should add a new directory', function () {
+      return put(joinedRoutes.directories, {
+        path: './src/server/test/fixtures/somemorelogs',
+        name: 'Test Fixture 3'
+      }).then(resp => {
+        assert(resp.data.success, 'failed to add directory')
+      })
+    })
+    it('should modify a directory', function () {
+
+    })
+    it('should delete a directory', function () {
+      return get(joinedRoutes.directories).then(resp => {
+        const delKey = resp.data.data.slice(-1)[0].key
+        del(joinedRoutes.directories, { key: delKey }).then(resp => {
+          assert(resp.data.success, 'failed to delete directory')
+        })
       })
     })
   })
