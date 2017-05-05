110c432cb2a833988dcd85c5afce36c827212bac
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-05-05 11:25:56 -0700

    Fix key error on loglines

--- src/client/components/loglines/loglines.js
+++ src/client/components/loglines/loglines.js
@@ -1,14 +1,10 @@
 import React from 'react'
-
 import { loglines as loglinesStyle } from './loglines.css'
-
 import Line from './line/line'
-
-const createLines = (loglines, i) => loglines.map(logline => <Line props={logline} key={i} />)
 
 const Loglines = ({loglines}) =>
   <ul className={loglinesStyle}>
-    {loglines ? createLines(loglines) : null}
+    { loglines.map((logline, i) => <Line props={logline} key={i} />) }
   </ul>
 
 export default Loglines
