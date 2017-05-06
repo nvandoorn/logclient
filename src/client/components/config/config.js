65c6ef4d93c5c0eeea035476d854bed91c55aa38
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-05-05 20:08:51 -0700

    Clear config component (WIP)

--- src/client/components/config/config.js
+++ src/client/components/config/config.js
@@ -1,39 +1,5 @@
-import React, { Component } from 'react'
-import { FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
+import React from 'react'
 
-import Modal from '../modal/modal'
-
-const ConfigDir = ({name, path}) =>
-  <form>
-    <FormGroup>
-      <ControlLabel>Log Folder Name</ControlLabel>
-      <FormControl
-        type='text'
-        value={name || ''}
-        placeholder='My Server Logs'
-      />
-    </FormGroup>
-    <FormGroup>
-      <ControlLabel>Log Folder Path</ControlLabel>
-      <FormControl
-        type='text'
-        value={path || ''}
-        placeholder='/var/log/'
-      />
-    </FormGroup>
-  </form>
-
-class Config extends Component {
-  render () {
-    console.log(this.props)
-    return (
-      <div>
-        <Modal show={this.props.show}>
-          {this.props.directories.map(dir => <ConfigDir {...dir} key={dir.path} />)}
-        </Modal>
-      </div>
-    )
-  }
-}
+const Config = props => <div>config goes here</div>
 
 export default Config
