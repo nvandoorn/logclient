730dc6ae62c47453d2543c6fb5ff43c3833ab536
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-05-07 23:20:39 -0700

    Cleanup import and eslint ignore line

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Layout from './layout/layout'
+import Layout from './layout/layout' // eslint-disable-line
 
-import './index.css.js'
+import './index.css'
 
 ReactDOM.render(
   <Layout />,
