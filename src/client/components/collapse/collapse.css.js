7f337cc737fe3c507489cc629666dd4d8ed7ba96
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-05-07 23:17:31 -0700

    Implement basic collapse component
    In hind sight I don't think this is needed at all. I will likely just
    append and remove the element to be collapsed instead. Committing these
    files incase I change my mind.

---
+++ src/client/components/collapse/collapse.css.js
@@ -0,0 +1,13 @@
+export const collapse = {
+  transition: 'height 0.5s',
+  overflow: 'hidden',
+  height: 0
+}
+
+export const open = {
+  height: 'auto'
+}
+
+export const closed = {
+  height: 0
+}
