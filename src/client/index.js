0268e7f561c52b86738f0cfa00bfdfdafe14e23a
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-05-11 16:25:32 -0700

    Add font-awesome css

--- src/client/index.js
+++ src/client/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import Layout from './layout/layout' // eslint-disable-line
 
 import './index.css'
+import '../../node_modules/font-awesome/css/font-awesome.min.css'
 
 ReactDOM.render(
   <Layout />,
