00ee59bb2cd57d12b535dc73d6942fc7cbb55363
Nick Van Doorn <vandoorn.nick@gmail.com>
2017-05-11 16:25:11 -0700

    Implement month switcher

--- src/client/components/datepicker/datepicker.js
+++ src/client/components/datepicker/datepicker.js
@@ -1,8 +1,20 @@
 import React from 'react'
 import Calendar from './calendar/calendar'
 
+import { Round as RoundBtn } from '../buttons/buttons'
+
+import { monthContainer, container } from './datepicker.css'
+
+const Month = props =>
+  <div className={monthContainer}>
+    <RoundBtn faName='arrow-left' onClick={() => { props.setMonth(props.moment.month() - 1 ) }} />
+    <div>{`${props.moment.format('MMM')} ${props.moment.year()}`}</div>
+    <RoundBtn faName='arrow-right' onClick={() => { props.setMonth(props.moment.month() + 1 ) }} />
+  </div>
+
 const DatePicker = props =>
-  <div>
+  <div className={container}>
+    <Month {...props} />
     <Calendar {...props} />
   </div>
 
